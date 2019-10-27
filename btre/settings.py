383f7ac2f1420300fac033afd1e5b53ea74f84e7
soheil-esi <48238829+soheil-esi@users.noreply.github.com>
2019-10-27 14:31:15 +0330

    added local settings

--- btre/settings.py
+++ btre/settings.py
@@ -147,3 +147,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'soheilesdi@gmail.com'
 EMAIL_HOST_PASSWORD = 'esi13780214'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
